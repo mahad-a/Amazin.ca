51d1457af5fc9c797abe7785dbc9c79b28aaa7ca
ilyaas <ilyaashussein123@gmail.com>
2024-11-10 01:23:36 -0500

    User loginPage implemented

--- app/src/main/java/com/example/app/UserController.java
+++ app/src/main/java/com/example/app/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +25,6 @@ public class UserController {
     public UserController() {}
 
 
-
-    
-
     /**
      * Endpoint to register a new user.
      *
@@ -46,6 +44,11 @@ public class UserController {
         } catch(Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginUser";
     }
 
     /**
