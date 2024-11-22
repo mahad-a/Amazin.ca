218f8a193eeeb706f642889d01613e4c60fb81eb
Mahad Ahmed <mahad.ahmed613@gmail.com>
2024-11-22 13:44:23 -0500

    added @Repository annotation due to error where none of the repositories would be autowired

--- app/src/main/java/com/example/app/UserRepository.java
+++ app/src/main/java/com/example/app/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.app;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
 
